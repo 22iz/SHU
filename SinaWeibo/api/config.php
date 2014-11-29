7ff21958e545780a7e86e5b6da178c2c571abcbb
isaacnotnwton <isaacnotnwton@gmail.com>
2014-11-30 01:56:43 +0800

    add SinaWeibo Login Eg

---
+++ SinaWeibo/api/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '2045616432' );
+define( "WB_SKEY" , '6ca8d7e09284ba02744e09aced7b8ab4' );
+define( "WB_CALLBACK_URL" , 'http://www.mckee.com/zend/SinaWeibo/api/callback.php' );
